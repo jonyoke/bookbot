1f1288284a357c117169c7fd729e69ad2a475f78
jonyoke <jonyoke@gmail.com>
2025-11-04 18:15:47 -0700

    end of bookbot

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from stats import get_num_words
+import sys
+from stats import *
 
 def get_book_text(path_to_file):
     file_contents = ""
@@ -6,12 +7,27 @@ def get_book_text(path_to_file):
         file_contents = f.read()
     return file_contents
 
-def main():
-    path_to_file ="./books/frankenstein.txt"
-
-    num_words = get_num_words(get_book_text(path_to_file))
-
-    print(f"Found {num_words} total words")
+def main():  
+    args = sys.argv
+    if len(args) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
     
+    #path_to_file ="./books/frankenstein.txt"
+    path_to_file = args[1]
+    print(path_to_file)
+
+    long_string = get_book_text(path_to_file)
+
+    num_words = get_num_words(long_string)
+    print(f"Found {num_words} total words")
+
+    sorted_character_counter = sort_character_counter(get_num_characters(long_string))
+
+    for dict in sorted_character_counter:
+        char = dict["char"]
+        num = dict["num"]
+        if char.isalpha():
+            print(f"{char}: {num}")
 
 main()
--- stats.py
+++ stats.py
@@ -1,2 +1,26 @@
 def get_num_words(long_string):
     return len(long_string.split())
+
+def get_num_characters(long_string):
+    lowercase_string = long_string.lower()
+    character_counter = {}
+    for i in range(0, len(lowercase_string)):
+        character = lowercase_string[i]
+        if character not in character_counter:
+            character_counter[character] = 1
+        else:
+            character_counter[character] = character_counter[character] + 1
+    return character_counter
+
+def return_num_from_dict(dict):
+    return dict["num"]
+
+def sort_character_counter(character_counter):
+    list_of_character_counters = []
+    for key, value in character_counter.items():
+        dict = {}
+        dict["char"] = key
+        dict["num"] = value
+        list_of_character_counters.append(dict)
+    #print(list_of_character_counters)
+    return sorted(list_of_character_counters, key=return_num_from_dict, reverse=True)
